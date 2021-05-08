ddfceb9fddaf40f69488e1f88255d4ab7c0ef002
AFOEK <felix.montalfu@gmail.com>
2021-05-08 21:10:21 +0700

    Version 0.0.6 (adding progress bar)

--- Shell_Selector_GUI.sh
+++ Shell_Selector_GUI.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/bash
-choice=$?
 eval `resize`
 whiptail --title "Welcome" --msgbox "This are bash script for download and change your shell terminal.\nIf you encounter a bugs or mistake please inform me in github :)" 10 45
-whiptail --title "Menus" --menu "What you want to do ? (REMINDER THIS ARE RUNNING AN PRE-CODED SCRIPT, YOU MUST KNOW WHAT YOU DOING." $LINES $COLUMNS $(( $LINES - 8 )) \
+choice=$(whiptail --title "Menus" --menu "What you want to do ? (REMINDER THIS ARE RUNNING AN PRE-CODED SCRIPT, YOU MUST KNOW WHAT YOU DOING." $LINES $COLUMNS $(( $LINES - 8 )) \
 "Exit" "exit from this programme" \
 "Check Shell" "check all installed shell in this system" \
 "Install Shell" "pick a shell you want to download" \
-"System Info" "check system information"
+"System Info" "check system information" 3>&1 1>&2 2>&3)
 
-case $choice in
-    "Exit")
+if [[ $choice == "Exit" ]]; then
     echo "Exited"
-    ;;
-    "Check Shell")
-    whiptail --gauge "Checking installed shell"
-esac
+    exit 0
+elif [[ $choice == "Check Shell" ]]; then
+    {
+        for((i=0;i<=100;i+=12)); do
+        sleep 0.1
+        echo $i
+        done
+    } |  whiptail --gauge "Checking installed shell" 6 50 0
+    whiptail --title "Shell installed" --msgbox /dev/stdout 10 45 <<<"$(cat /etc/shells | grep -i '/usr/bin/' /etc/shells)"
+fi
